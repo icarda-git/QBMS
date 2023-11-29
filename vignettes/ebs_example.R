f6267e0cb63efabf9f33f9286fd3b2c4a66b5a6f
Khaled Al-Shamaa <khaled.alshamaa@gmail.com>
2023-11-29 09:51:00 +0200

    ESD 4613 and 4615 resolved and tested

--- vignettes/ebs_example.R
+++ vignettes/ebs_example.R
@@ -21,11 +21,44 @@ ebs$config <- list(crop      = '',
 
 ebs$state  <- list(token = '', 
                    user = '', 
-                   expires_in = as.numeric(Sys.time()) + 3600)
+                   expires_in = 3600)
 
-# https://cbbrapi-qa.ebsproject.org/brapi/v2/auth/login
-ebs$state$token <- readline('token:')
+################################################################################
+#' EBS OAuth 2.0
+#' https://cbbrapi-qa.ebsproject.org/brapi/v2/auth/login
+#'
+#' Package maintainers might want to build this app in as a fallback, possibly 
+#' taking some measures to obfuscate the client ID and secret and limit its use 
+#' to your package.
+#' 
+#' Note that three-legged OAuth always requires the involvement of a user, so 
+#' the word “secret” here can be somewhat confusing. It is not a secret in the 
+#' same sense as a password or token. But you probably still want to store it 
+#' in an opaque way, so that someone else cannot easily “borrow” it and present 
+#' an OAuth consent screen that impersonates your package.
+#' 
+#' The Client ID is a public identifier of your application, it is not a secret 
+#' anyway and any end user can see what it is when the app redirects them to 
+#' sign in (e.g., if they use browser tools to view the HTTP request).
+
+ebs$state$client_id     <- readline('Client ID: ')
+ebs$state$client_secret <- readline('Client Secret: ')
+
+app <- httr::oauth_app(appname = 'EBS', 
+                       key = ebs$state$client_id, 
+                       secret = ebs$state$client_secret,
+                       redirect_uri = 'http://localhost:1410')
+
+endpoint <- httr::oauth_endpoint(authorize = 'https://auth-dev.ebsproject.org/oauth2/authorize',
+                                 access = 'https://auth-dev.ebsproject.org/oauth2/token')
+
+token <- httr::oauth2.0_token(endpoint, app)
+
+token$credentials$id_token
+
+# ebs$state$token <- readline('token:')
 ebs$state$token <- token$credentials$id_token
+ebs$state$expires_in <- token$credentials$expires_in
 
 # JSON Web Tokens are an open, industry standard RFC 7519 method for representing 
 # claims securely between two parties.
@@ -53,14 +86,7 @@ info <- get_study_info()
 # The requested feature will be available at the end of October or November at the latest.
 data <- get_study_data()
 
-# does not restrict by study id /brapi/v2/germplasm?studyDbId=
-# https://ebsproject.atlassian.net/servicedesk/customer/portal/2/ESD-4613
-# https://ebsproject.atlassian.net/browse/ESD-4613
-# The requested feature will be available at the end of October or November at the latest.
 germplasm <- get_germplasm_list()
-
-# temp fix to get the study germplasm list
-germplasm <- germplasm[germplasm$germplasmDbId %in% unique(data$germplasmDbId),]
 
 MET <- get_trial_data()
 
@@ -99,39 +125,3 @@ MET <- get_trial_data()
 
 #' call the /brapi/v2/observations/table?germplasmDbId= endpoint generate error 500
 # get_germplasm_data()
-
-
-################################################################################
-# EBS OAuth 2.0
-# https://cbbrapi-qa.ebsproject.org/brapi/v2/auth/login
-# https://ebsproject.atlassian.net/servicedesk/customer/portal/2/ESD-4615
-# https://ebsproject.atlassian.net/browse/ESD-4615
-
-#' Package maintainers might want to build this app in as a fallback, possibly 
-#' taking some measures to obfuscate the client ID and secret and limit its use 
-#' to your package.
-#' 
-#' Note that three-legged OAuth always requires the involvement of a user, so 
-#' the word “secret” here can be somewhat confusing. It is not a secret in the 
-#' same sense as a password or token. But you probably still want to store it 
-#' in an opaque way, so that someone else cannot easily “borrow” it and present 
-#' an OAuth consent screen that impersonates your package.
-#' 
-#' The Client ID is a public identifier of your application, it is not a secret 
-#' anyway and any end user can see what it is when the app redirects them to 
-#' sign in (e.g., if they use browser tools to view the HTTP request).
-
-ebs$state$client_id     <- readline('Client ID: ')
-ebs$state$client_secret <- readline('Client Secret: ')
-
-app <- httr::oauth_app(appname = 'EBS', 
-                       key = ebs$state$client_id, 
-                       secret = ebs$state$client_secret,
-                       redirect_uri = 'http://localhost:1410')
-
-endpoint <- httr::oauth_endpoint(authorize = 'https://auth-dev.ebsproject.org/oauth2/authorize',
-                                 access = 'https://auth-dev.ebsproject.org/oauth2/token')
-
-token <- httr::oauth2.0_token(endpoint, app)
-
-token$credentials$id_token
