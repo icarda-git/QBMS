379d82bf904a2c3c1b77c5800800b05bc51d4857
Khaled Al-Shamaa <khaled.alshamaa@gmail.com>
2023-12-02 10:55:39 +0200

    use brapi_map[] in get_study_data()

--- R/qbms.R
+++ R/qbms.R
@@ -1145,16 +1145,13 @@ get_study_data <- function() {
   if (is.null(qbms_globals$state$study_db_id)) {
     stop("No study has been selected yet! You have to set your study first using the `set_study()` function")
   }
-
-  crop_path <- ifelse(qbms_globals$config$crop == "", "", paste0("/", qbms_globals$config$crop))
   
-  if (qbms_globals$config$brapi_ver == "v2") {
-    call_url <- paste0(qbms_globals$config$base_url, crop_path,
-                       "/brapi/v2/observations/table?studyDbId=", qbms_globals$state$study_db_id)
-  } else {
-    crop_url <- paste0(qbms_globals$config$base_url, crop_path, "/brapi/v1")
-    call_url <- paste0(crop_url, "/studies/", qbms_globals$state$study_db_id, "/table")
-  }
+  call_url <- paste0(qbms_globals$config$base_url, 
+                     ifelse(qbms_globals$config$crop == "", "", paste0("/", qbms_globals$config$crop)), 
+                     "/brapi/", qbms_globals$config$brapi_ver, "/", 
+                     brapi_map[brapi_map$func_name == "get_study_data" & brapi_map$brapi_ver == qbms_globals$config$brapi_ver, "brapi_call"])
+  
+  call_url <- sub("\\{studyDbId\\}", qbms_globals$state$study_db_id, call_url)
 
   study_result <- brapi_get_call(call_url)
   study_data   <- as.data.frame(study_result$data)
