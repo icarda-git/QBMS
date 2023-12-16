55364ced32c758318c9bbe13db52206b8de94454
Khaled Al-Shamaa <khaled.alshamaa@gmail.com>
2023-12-16 15:07:31 +0100

    Update ebs_example.R

--- vignettes/ebs_example.R
+++ vignettes/ebs_example.R
@@ -11,28 +11,6 @@ set_qbms_config(url = 'https://cbbrapi-qa.ebsproject.org', engine = 'ebs', brapi
 
 # set_token(readline('token:'))
 
-################################################################################
-#' EBS OAuth 2.0
-#'
-#' Package maintainers might want to build this app in as a fallback, possibly 
-#' taking some measures to obfuscate the client ID and secret and limit its use 
-#' to your package.
-#' 
-#' Note that three-legged OAuth always requires the involvement of a user, so 
-#' the word “secret” here can be somewhat confusing. It is not a secret in the 
-#' same sense as a password or token. But you probably still want to store it 
-#' in an opaque way, so that someone else cannot easily “borrow” it and present 
-#' an OAuth consent screen that impersonates your package.
-#' 
-#' The Client ID is a public identifier of your application, it is not a secret 
-#' anyway and any end user can see what it is when the app redirects them to 
-#' sign in (e.g., if they use browser tools to view the HTTP request).
-#' 
-#' JSON Web Tokens are an open, industry standard RFC 7519 method for representing 
-#' claims securely between two parties.
-#'
-#' https://jwt.io/ allows you to decode, verify and generate JWT.
-
 login_oauth2(authorize_url = 'https://auth-dev.ebsproject.org/oauth2/authorize', 
              access_url    = 'https://auth-dev.ebsproject.org/oauth2/token', 
              client_id     = '5crahiqorgj0lppt3n9dkulkst', 
@@ -62,10 +40,6 @@ germplasm <- get_germplasm_list()
 ontology <- get_trial_obs_ontology()
 
 MET <- get_trial_data()
-
-#' implement the /brapi/v2/locations endpoint
-# list_locations()
-# get_program_studies()
 
 #' implement the /brapi/v2/germplasm?germplasmName= endpoint
 # get_germplasm_id()
